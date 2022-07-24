95baa87c2e830d687e0bcd351859b5511773c471
Echo Barros <diegobarros@mail.weber.edu>
2022-07-24 14:44:17 -0600

    modular implementation of fetching, splitting, and shuffling the hardest sudokus

--- lib/sudoku_puzzle.dart
+++ lib/sudoku_puzzle.dart
@@ -3,22 +3,42 @@ import 'dart:convert';
 import 'dart:async';
 import 'dart:math';
 
-void main() async {
+List<int> splitSudoku(String line) {
+  var sudoku = List<int>.filled(163, 0);
+  for (int i = 0; i < 9 * 9 + 9 * 9 + 1; ++i) {
+/*    if (i == 9 * 9) {
+      // skip the colon. Compiler optimizes this
+      continue; // this doesn't work for some reason ???
+    }*/
+    sudoku[i] = int.tryParse(line[i]) ?? 0;
+  }
+  return sudoku;
+}
+
+List<int> shuffleSudoku(List<int> sudoku) {
   var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   numbers.shuffle();
-  var lineNum = Random().nextInt(373) + 3;
-
-  final file = File('assets/puzzles6_forum_hardest_1106.solution');
-  Stream<String> lines = file
-      .openRead()
-      .transform(utf8.decoder) // Decode bytes to UTF-8.
-      .transform(LineSplitter()); // Convert stream to individual lines.
-  try {
-    await for (var line in lines) {
-      print('$line: ${line.length} characters');
+  for (int num in sudoku) {
+    if (num == 0) {
+      continue;
     }
-    print('File is now closed.');
-  } catch (e) {
-    print('Error: $e');
+    num = numbers[num - 1];
   }
+  // TODO shuffle columns and rows that don't cross box boundaries
+  // TODO 90/270 degree rotations
+  // https://ljkrakauer.com/Sudoku/transformations.htm
+  return sudoku;
+}
+
+void main() async {
+  const linesInFile = 376;
+  const headerLines = 2;
+  // var lineNum = Random().nextInt(373) + headerLines;
+  var lineNum = 97;
+
+  final file =
+      File('assets/puzzles6_forum_hardest_1106.solution').readAsStringSync();
+  const splitter = LineSplitter();
+  final string = splitter.convert(file);
+  final sudokuString = splitSudoku(string[lineNum]);
 }
