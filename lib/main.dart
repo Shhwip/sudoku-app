8ccd5ef3cb3d073cb73da2380b08792580932ed2
Echo Barros <diegobarros@mail.weber.edu>
2022-07-30 15:55:32 -0600

    Removed comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,7 @@ import 'sudoku_puzzle.dart';
 
 void main() {
   runApp(
-    // Provide the model to all widgets within the app. We're using
-    // ChangeNotifierProvider because that's a simple way to rebuild
-    // widgets when a model changes. We could also just use
-    // Provider, but then we would have to listen to Counter ourselves.
-    //
-    // Read Provider's docs to learn about all the available providers.
     ChangeNotifierProvider(
-      // Initialize the model in the builder. That way, Provider
-      // can own Counter's lifecycle, making sure to call `dispose`
-      // when not needed anymore.
       create: (context) => SudokuModel(),
       child: const MyApp(),
     ),
@@ -51,7 +42,7 @@ class MyStatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    Sudoku sudoku = Sudoku(Difficulty.easy);
+    Sudoku sudoku = Sudoku(Difficulty.hard);
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
@@ -424,7 +415,7 @@ class _NumBoxState extends State<NumBox> {
 }
 
 class SudokuModel extends ChangeNotifier {
-  final curSudoku = List<String>.filled(81, ' ');
+  Sudoku curSudoku = Sudoku(Difficulty.impossible);
 }
 
 class SizeConfig {
